466d749e0b5985204e4d1c6e3e4252ea71fcbaeb
Florian Dreschner <Florian@Florians-MacBook-Pro-8.local>
2014-11-24 18:12:47 +0100

    - Added images and texts to page - Edited navbar to have three options

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,37 @@
  *
  * Main module of the application.
  */
-angular
-  .module('mocitApp', [
-    'ngAnimate',
-    'ngTouch'
-  ]);
+var myApp = angular.module('mocitApp', [
+	'ngAnimate',
+	'ngTouch',
+	'ui.router'
+	]);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+	//
+	// Now set up the states
+	$stateProvider
+	.state('information', {
+		url: '/',
+		templateUrl: 'views/_information.html'
+	})
+	.state('development', {
+		url: '/development',
+		templateUrl: 'views/_development.html'
+	})
+	.state('idea', {
+		url: '/idea',
+		templateUrl: 'views/_idea.html'
+	})
+	.state('name', {
+		url: '/name',
+		templateUrl: 'views/_name.html'
+	})
+	.state('organisation', {
+		url: '/organisation',
+		templateUrl: 'views/_organisation.html'
+	}).state('media', {
+		url: '/media',
+		templateUrl: 'views/_media.html'		
+	});
+});
